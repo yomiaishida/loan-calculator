1f04fe22f7b9e2f650f905ed2e706506695058aa
Aishida Abayomi <yomicoperate@gmail.com>
2020-01-13 12:33:30 +0100

    Added error handling to the UI

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+// Clear error
+const clearError = () => {
+  document.querySelector(".alert").remove();
+};
+
+// Show Error
+const showError = error => {
+  // Create a div
+  const errorDiv = document.createElement("div");
+
+  // Get Elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  // Add class
+  errorDiv.className = "alert alert-danger";
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error before heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+};
+
 // Calculate Result
 const calculateResults = e => {
   // UI vars
@@ -22,7 +49,7 @@ const calculateResults = e => {
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
   } else {
-    console.log("Please check your numbers");
+    showError("Please check your numbers");
   }
 
   e.preventDefault();
