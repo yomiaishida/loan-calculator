bed70707adcf07724476f55da3c0c600c153da58
Aishida Abayomi <yomicoperate@gmail.com>
2020-01-13 12:11:40 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Calculate Result
+const calculateResults = e => {
+  // UI vars
+
+  const UIamount = document.getElementById("amount");
+  const UIinterest = document.getElementById("interest");
+  const UIyears = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(UIamount.value);
+  const calculatedInterest = parseFloat(UIinterest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(UIyears.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    console.log("Please check your numbers");
+  }
+
+  e.preventDefault();
+};
+
+// Listen for submit
+document
+  .getElementById("loan-form")
+  .addEventListener("submit", calculateResults);
